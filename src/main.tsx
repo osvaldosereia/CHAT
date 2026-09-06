8f368ae36a25e89530089d58b3dacd6108252adc
OSSJU <Junior.sereia@hotmail.com>
2026-09-06 12:47:16 -0400

    style: load settings styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './admin-modules.css'
+import './settings.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
